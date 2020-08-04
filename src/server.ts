f2f0a2960e480b60d5b53118b80059d08a2764d9
sambuite <murilosambuite@gmail.com>
2020-08-04 14:24:16 -0300

    feat: add routes; add cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
 app.listen(3333, () => {
-  console.log('Server up!');
+  console.log('Server up 🔥');
 });
